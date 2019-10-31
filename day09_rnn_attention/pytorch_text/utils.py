084e20bad9547d2c7d00dac76040129bede0508a
lovit <soy.lovit@gmail.com>
2019-10-31 14:10:56 +0900

    Random selector

--- day09_rnn_attention/pytorch_text/utils.py
+++ day09_rnn_attention/pytorch_text/utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from time import time, strftime, gmtime
 
 
@@ -7,3 +8,14 @@ installpath = os.path.dirname(__file__)
 def time_from_t(base):
     t = time() - base
     return strftime("%H:%M:%S", gmtime(t))
+
+def random_select(namedata):
+    """
+    namedata : [(category, [name, name, ... ]), 
+                (category, [name, name, ... ]).
+                ... ]
+    return : name, category
+    """
+    category, names = namedata[np.random.randint(len(namedata))]
+    name = names[np.random.randint(len(names))]
+    return name, category
